13908fd7d21e279c511c1d956895af159bf7b5e2
RonaldPondo <ronaldpondo@outlook.com>
2023-04-10 02:31:48 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * get_endianness - Write a function that checks the endianness.
- * Prototype: int get_endianness(void).
- * Return: 1, if architecture is little endian, 0 in case of big endian.
+ * get_endianness - return the endianness of the machine
+ *
+ * Return: 0 for big endian, 1 for little endian
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+	int n = 1;
 
-	return (*c);
+	return (*((char *) &n) + '0');
 }
